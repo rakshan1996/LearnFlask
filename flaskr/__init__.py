6129c4a3bd4c763af95b8f3ef108c195d5fb6781
Rakshan Thapar <rakshan.thapar@cygrp.com>
2019-08-06 18:38:12 +0530

    Final Commit For Today

---
+++ flaskr/__init__.py
@@ -0,0 +1,28 @@
+import os
+from flask import Flask
+
+def create_app(test_config=None):
+	#create and configure the app
+	app= Flask(__name__,instance_relative_config=True)
+	app.config.from_mapping(
+		SECRET_KEY='dev',
+		DATABASE=os.path.join(app.instance_path,'flaskr.sqlite')
+	)
+
+	if test_config is None:
+		#Load the instance config,if it exists when not testing
+		app.config.from_pyfile('config.py',silent=True)
+	else:
+		app.config.from_mapping(test_config)
+
+	#ensure the instance folder exsists
+
+	try: 
+		os.makedirs(app.instance_path)
+	except OSError:
+		pass
+
+
+@app.route('/')
+def hello_world():
+	return 'Hello Devil, Welcome to the world of Python.'
--- hello.py
+++ hello.py
@@ -4,3 +4,11 @@ app = Flask(__name__)
 @app.route('/')
 def hello_world():
 	return 'Hello Devil, Welcome to the world of Python.'
+
+@app.route('/routesCheck/')
+def route_check():
+		return "route"
+
+@app.route('/about')
+def about():
+		return "testComplete"
