75945a4dc3d0789bcfc0cbb5d3968d3e031d47e5
Rakshan Thapar <rakshan.thapar@cygrp.com>
2019-08-07 19:01:18 +0530

    Blog index created Rest In Progress

--- flaskr/__init__.py
+++ flaskr/__init__.py
@@ -2,6 +2,7 @@ import os
 from flask import Flask
 from . import db
 from . import auth
+from . import blog
 
 
 
@@ -39,4 +40,6 @@ def create_app(test_config=None):
 
 	db.init_app(app)
 	app.register_blueprint(auth.bp)
+	app.register_blueprint(blog.bp)
+	app.add_url_rule('/',endpoint="index")
 	return app
---
+++ flaskr/blog.py
@@ -0,0 +1,30 @@
+from flask import (
+	Blueprint,flash,g,redirect,render_template,request,session,url_for
+)
+from werkzeug.exceptions import abort
+
+from flaskr.auth import login_required
+from flaskr.db import get_db
+
+bp = Blueprint('blog', __name__)
+
+@bp.route('/')
+def index():
+	db=get_db()
+	posts=db.execute(
+		'Select p.id,title,body,created,author_id,username'
+		'From post p JOIN user u ON p.author_id=u.id'
+		'ORDER BY created DESC'
+		).fetchAll()
+	return render_template('blog/index.html',posts=posts)
+
+@bp.route('/create',methods=('GET','POST'))
+@login_required
+def create():
+	if request.method == 'POST':
+		title=request.form['title']
+		body=request.form['body']
+		error=None
+
+		if title 
+
